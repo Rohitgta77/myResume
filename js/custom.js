75ff3f979208b19a7174442f7a33a8839136af72
Rohit Gupta <rohitgta77@gmail.com>
2020-08-05 17:45:42 +0530

    Change Backgroung images, Download Cv Link
    Change Backgroung images,
    Download Cv Link

--- js/custom.js
+++ js/custom.js
@@ -10,9 +10,11 @@ $(window).load(function(){
 $(function(){
     jQuery(document).ready(function() {
 		$('body').backstretch([
-	 		 "images/tm-bg-slide-1.jpg", 
+	 		 "images/tm-bg-slide-1.jpg",	 		  
 	 		 "images/tm-bg-slide-2.jpg",
-			 "images/tm-bg-slide-3.jpg"
+			 "images/tm-bg-slide-3.jpg",
+			 "images/tm-bg-slide-4.jpg",
+			 "images/tm-bg-slide-1.jpg"
 	 			], 	{duration: 3200, fade: 1300});
 		});
 })
