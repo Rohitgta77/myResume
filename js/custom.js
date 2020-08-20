059b94966bc243c28c5c771cefdd327796120a6f
Rohit Gupta <rohitgta77@gmail.com>
2020-08-20 13:31:29 +0530

    changes

--- js/custom.js
+++ js/custom.js
@@ -12,9 +12,10 @@ $(function(){
 		$('body').backstretch([
 	 		 "images/tm-bg-slide-1.jpg",	 		  
 	 		 "images/tm-bg-slide-2.jpg",
-			 "images/tm-bg-slide-3.jpg",
-			 "images/tm-bg-slide-4.jpg",
-			 "images/tm-bg-slide-1.jpg"
 	 			], 	{duration: 3200, fade: 1300});
 		});
 })
+
+			 //"images/tm-bg-slide-3.jpg",
+			 //"images/tm-bg-slide-4.jpg",
+			 //"images/tm-bg-slide-1.jpg"
