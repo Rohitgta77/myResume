8b31276fca660afbd6af3bb914a25168e2b41174
Rohit Gupta <rohitgta77@gmail.com>
2020-08-21 12:27:53 +0530

    Commit id 001  21 Aug 2020

--- js/custom.js
+++ js/custom.js
@@ -10,12 +10,12 @@ $(window).load(function(){
 $(function(){
     jQuery(document).ready(function() {
 		$('body').backstretch([
-	 		 "images/tm-bg-slide-1.jpg",	 		  
-	 		 "images/tm-bg-slide-2.jpg",
+	 		 "images/tm-bg-slide-5.jpg" 		  
+	 		 
 	 			], 	{duration: 3200, fade: 1300});
 		});
 })
-
+			 //"images/tm-bg-slide-2.jpg",
 			 //"images/tm-bg-slide-3.jpg",
 			 //"images/tm-bg-slide-4.jpg",
 			 //"images/tm-bg-slide-1.jpg"
